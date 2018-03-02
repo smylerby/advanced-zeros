bd69db18cc6f3de9843d2e7b8515ae2aa4404678
Rustam Shorov <srmparusa@gmail.com>
2018-03-02 12:59:57 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 module.exports = function getZerosCount(number, base) {
 
-  let count = number;
-  let Dig = base;
+  let nulls = number;
+  let ss = base;
   let a, c, d = 0;
 
   for (let i = 2; i <= base; i++) {
       c = 0;
       d = number;
 
-      if (Dig % i == 0) {
+      if (ss % i == 0) {
       a = 0;
 
-      while (Dig % i == 0) {
+      while (ss % i == 0) {
         a++;
-        Dig = Math.floor(Dig/i);
+        ss = Math.floor(ss/i);
       }
 
       while (d/i > 0) {
-        c = c + Math.floor(d/i);
+        c += Math.floor(d/i);
         d = Math.floor(d/i);
       }
-      count = Math.floor(Math.min(count, c/a));
+      nulls = Math.floor(Math.min(nulls, c/a));
     }
   }
-  return count;
+  return nulls;
 }
