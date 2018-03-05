db0155fff40b976dd129f01b0c40f15a8700fe76
Rustam Shorov <srmparusa@gmail.com>
2018-03-05 19:19:53 +0100

    better

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 module.exports = function getZerosCount(number, base) {
 
-  let nulls = number;
-  let ss = base;
-  let a, c, d = 0;
+    let nulls = number;
+    let s_s = base;
 
-  for (let i = 2; i <= base; i++) {
-      c = 0;
-      d = number;
+    for (let i = 2; i <= base; i++) {
+        if (s_s % i == 0) {
+            let divisor = 0;
+            while (s_s % i == 0) {
+                divisor++;
+                s_s = Math.floor(s_s/i);
+            }
 
-      if (ss % i == 0) {
-      a = 0;
+            let rest = 0;
+            let rest_of_fac = number;
 
-      while (ss % i == 0) {
-        a++;
-        ss = Math.floor(ss/i);
-      }
-
-      while (d/i > 0) {
-        c += Math.floor(d/i);
-        d = Math.floor(d/i);
-      }
-      nulls = Math.floor(Math.min(nulls, c/a));
+            while (rest_of_fac/i > 0) {
+                rest += Math.floor(rest_of_fac/i);
+                rest_of_fac = Math.floor(rest_of_fac/i);
+            }
+            nulls = Math.floor(Math.min(nulls, rest/divisor));
+        }
     }
-  }
-  return nulls;
+    return nulls;
 }
